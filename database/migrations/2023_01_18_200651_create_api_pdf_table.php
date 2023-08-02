595dc0599b6844514ae1185c6a5599feaf38dec6
ItsJiran <its.gilangramadhan@gmail.com>
2023-08-02 11:51:33 +0000

    test

--- database/migrations/2023_01_18_200651_create_api_pdf_table.php
+++ database/migrations/2023_01_18_200651_create_api_pdf_table.php
@@ -8,26 +8,26 @@ return new class extends Migration
 {
     /**
      * Run the migrations.
+     *
+     * @return void
      */
-    public function up(): void
+    public function up()
     {
-        Schema::create('personal_access_tokens', function (Blueprint $table) {
-            $table->id();
-            $table->morphs('tokenable');
-            $table->string('name');
+        Schema::create('api_pdf', function (Blueprint $table) {
+            $table->bigInteger('user_id');
             $table->string('token', 64)->unique();
-            $table->text('abilities')->nullable();
             $table->timestamp('last_used_at')->nullable();
             $table->timestamp('expires_at')->nullable();
-            $table->timestamps();
         });
     }
 
     /**
      * Reverse the migrations.
+     *
+     * @return void
      */
-    public function down(): void
+    public function down()
     {
-        Schema::dropIfExists('personal_access_tokens');
+        Schema::dropIfExists('api_pdf');
     }
 };
