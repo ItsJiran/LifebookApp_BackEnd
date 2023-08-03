1adfe95d2a9dfeacceb5817cc2a0a77b00870ff0
ItsJiran <its.gilangramadhan@gmail.com>
2023-08-03 18:58:09 +0000

    removing icon libray

--- resources/views/layouts/auth.blade.php
+++ resources/views/layouts/auth.blade.php
@@ -7,8 +7,8 @@
         <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
         <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@200;300;400;500;600;700;800&display=swap" rel="stylesheet">
 
+        <link rel='stylesheet' href='https://cdn.jsdelivr.net/npm/iconsax-font-icon@1.1.0/dist/icons.min.css'/>
         <link rel='stylesheet' type='text/css' href='/public/library/iconsax/style.css'/>
-        <link rel='stylesheet' type='text/css' href='/public/assets/css/style.css'/>
     </head>
     <body class='auth-page'>
 
