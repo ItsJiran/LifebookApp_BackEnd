ab03835c525a77cff09336a011bb0e0784c8fd1b
ItsJiran <its.gilangramadhan@gmail.com>
2023-08-07 05:23:40 +0700

    ad

--- public/assets/js/script.navigation.confirmback.js
+++
@@ -1,7 +0,0 @@
-history.pushState(null,null,window.top.location.pathname + window.top.location.search);
-window.addEventListener('popstate',(e)=>{
-    e.preventDefault();
-    var r = confirm('Yakin anda ingin pergi ?');
-    if(r) history.back();
-    history.pushState(null, null, window.location.pathname);
-},false);
